14af2a4501ebc50c4f204aee20d3c3e26b248d94
Islam Ibakaev <islamibakaev@gmail.com>
2025-02-01 00:21:04 +0300

    added hero section for home page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,27 +1,51 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { HeroSection } from "@/components/custom/hero-section";
+import qs from "qs";
 
+const homePageQuery = qs.stringify({
+  populate: {
+    blocks: {
+      on: {
+        "layout.hero-section": {
+          populate: {
+            image: {
+              fields: ["url", "alternativeText"]
+            },
+            link: {
+              populate: true
+            }
+          }
+        }
+      }
+    }
+  },
+});
 
-async function getStrapiData(url: string) {
-  const baseUrl = "http://localhost:1337"
+async function getStrapiData(path: string) {
+  const baseUrl = "http://localhost:1337";
+
+  const url = new URL(path, baseUrl);
+  url.search = homePageQuery;
+
   try {
-    const response = await fetch(baseUrl + url)
-    const data = await response.json()
-    return data
+    const response = await fetch(url.href);
+    const data = await response.json();
+    return data;
   } catch (error) {
-    console.log(error)
+    console.error(error);
   }
 }
 
 export default async function HomePage() {
-  const { data: { title, description } } = await getStrapiData("/api/home-page")
+  // const { data: { title, description, blocks } } = await getStrapiData("/api/home-page")
+  const strapiData = await getStrapiData("/api/home-page");
+
+  console.dir(strapiData, { depth: null });
+
+  const { title, description, blocks } = strapiData.data;
+
   return (
-    <main className="container mx-auto py-6">
-      <h1 className="text-5xl font-bold">{title}</h1>
-      <p className="text-xl mt-4">{description}</p>
-      <Button>Our Cool Button</Button>
-      <Link href="/about-us">О нас</Link>
-      <Link href="/projects">Проекты</Link>
+    <main>
+      <HeroSection data={blocks[0]} />
     </main>
   );
 }
