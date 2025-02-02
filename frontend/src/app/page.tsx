ee005aee32f36ca3c9e3a0f368800e8583fbc78d
Islam Ibakaev <islamibakaev@gmail.com>
2025-02-03 02:30:08 +0300

    display features section

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,61 +1,20 @@
+import { getHomePageData } from "@/data/loaders";
+
 import { HeroSection } from "@/components/custom/hero-section";
-import { getStrapiURL } from "@/lib/utils";
-import qs from "qs";
-
-const homePageQuery = qs.stringify({
-  populate: {
-    blocks: {
-      on: {
-        "layout.hero-section": {
-          populate: {
-            image: {
-              fields: ["url", "alternativeText"]
-            },
-            link: {
-              populate: true
-            }
-          }
-        },
-        "layout.features-section": {
-          populate: {
-            feature: {
-              populate: true,
-            },
-          },
-        },
-      }
-    }
-  },
-});
-
-async function getStrapiData(path: string) {
-  const baseUrl = getStrapiURL()
-
-  const url = new URL(path, baseUrl);
-  url.search = homePageQuery;
-
-  console.log(url.href);
-
-  try {
-    const response = await fetch(url.href);
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error(error);
-  }
-}
+import { FeatureSection } from "@/components/custom/features-section";
 
 export default async function HomePage() {
-  // const { data: { title, description, blocks } } = await getStrapiData("/api/home-page")
-  const strapiData = await getStrapiData("/api/home-page");
+  const strapiData = await getHomePageData();
+  const { blocks } = strapiData?.data || [];
+  return <main>{blocks.map(blockRenderer)}</main>;
+}
 
-  console.dir(strapiData, { depth: null });
+const blockComponents = {
+  "layout.hero-section": HeroSection,
+  "layout.features-section": FeatureSection,
+};
 
-  const { title, description, blocks } = strapiData.data;
-
-  return (
-    <main>
-      <HeroSection data={blocks[0]} />
-    </main>
-  );
+function blockRenderer(block: any) {
+  const Component = blockComponents[block.__component as keyof typeof blockComponents];
+  return Component ? <Component key={block.id} data={block} /> : null;
 }
